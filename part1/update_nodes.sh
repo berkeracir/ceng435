b5adbaa8fee22ae27e265b674c3d65ca97535ced
Berker ACIR <berkeracir159@gmail.com>
2018-11-30 14:32:20 +0300

    add ssh-add <SSH-KEY>

--- part1/update_nodes.sh
+++ part1/update_nodes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USERNAME=$1
+SSH_KEY=$2
 
 S="pc1.instageni.hawaii.edu"
 S_PORT=26014
@@ -14,11 +15,20 @@ D="pc1.instageni.hawaii.edu"
 D_PORT=26011
 
 if [ -n "$USERNAME" ]; then
-	scp -P $S_PORT source.py $USERNAME@$S:/users/$USERNAME
-	scp -P $B_PORT broker.py $USERNAME@$B:/users/$USERNAME
-	scp -P $R1_PORT router.py $USERNAME@$R1:/users/$USERNAME
-	scp -P $R2_PORT router.py $USERNAME@$R2:/users/$USERNAME
-	scp -P $D_PORT destionation.py $USERNAME@$D:/users/$USERNAME
+	if [ -n "$SSH_KEY" ]; then
+		if [ -f "$SSH_KEY" ]; then
+			ssh-add $SSH_KEY
+			scp -i $SSH_KEY -P $S_PORT source.py $USERNAME@$S:/users/$USERNAME
+			scp -i $SSH_KEY -P $B_PORT broker.py $USERNAME@$B:/users/$USERNAME
+			scp -i $SSH_KEY -P $R1_PORT router.py $USERNAME@$R1:/users/$USERNAME
+			scp -i $SSH_KEY -P $R2_PORT router.py $USERNAME@$R2:/users/$USERNAME
+			scp -i $SSH_KEY -P $D_PORT destionation.py $USERNAME@$D:/users/$USERNAME
+		else
+			echo "SSH Key with path: $2 does not exist!"
+		fi
+	else
+		echo "Missing Parameter: ${0##*/} $1 <SSH_KEY>"
+	fi
 else
-	echo "Missing parameter: ${0##*/} USERNAME"
+	echo "Missing Parameter: ${0##*/} <USERNAME> <SSH_KEY>"
 fi
