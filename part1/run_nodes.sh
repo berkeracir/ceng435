37f66b0fe48785678994fdd5a778916a184a3285
Berker ACIR <berkeracir159@gmail.com>
2018-11-30 20:26:47 +0300

    add PORTs as a variable

--- part1/run_nodes.sh
+++ part1/run_nodes.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+R1_PORT = 10001
+R2_PORT = 10002
+
 #gnome-terminal -x bash -c "clean; echo destination.py; echo "============="; python $PWD/destination.py"
-gnome-terminal -x bash -c "echo 1-router.py; echo "========="; python $PWD/router.py 10001"
-gnome-terminal -x bash -c "echo 2-router.py; echo "========="; python $PWD/router.py 10002"
+gnome-terminal -x bash -c "echo 1-router.py; echo "========="; python $PWD/router.py $R1_PORT"
+gnome-terminal -x bash -c "echo 2-router.py; echo "========="; python $PWD/router.py $R2_PORT"
 gnome-terminal -x bash -c "echo broker.py; echo "============="; python $PWD/broker.py"
 gnome-terminal -x bash -c "echo source.py; echo "========="; python $PWD/source.py"
 clear; echo destination.py; echo "============="; python $PWD/destination.py
