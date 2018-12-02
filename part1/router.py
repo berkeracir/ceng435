13a3c233ba0eaaf4a51d0dc33f9f382e660a2c3b
Berker ACIR <berkeracir159@gmail.com>
2018-12-02 12:46:43 +0300

    add comments

--- part1/router.py
+++ part1/router.py
@@ -1,24 +1,38 @@
 import socket
 import sys
 
+
+# Initialize buffer size for socket
 SOCKET_SIZE = 1024
 
+# Create socket with UDP connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Check whether we get expected arguments.If we do not get, exit from script
+# We need to get Router&Destination IP and Port number
+# Note that we gave this arguments in our script which executes all nodes
 if len(sys.argv) < 5:
     print "Expected Arguments:", sys.argv[0], "<ROUTER-IP>", "<ROUTER-PORT>", "<DESTINATION-IP>", "<DESTINATION-PORT>"
     sys.exit()
 
+# Assign argv variables to router address and destination address
 router_address = (sys.argv[1], int(sys.argv[2]))
 destination_address = (sys.argv[3], int(sys.argv[4]))
 
+# Bind the socket to address router address
+# In case of exception, exit from script
 try:
     sock.bind(router_address)
+
+    # Get continous packets
+    while True:
+        # Receive data from UDP connection
+        data, address = sock.recvfrom(SOCKET_SIZE)
+        print "Message:", data, "\n\tfrom:", address
+        # Send data to destination
+        sent = sock.sendto(data, destination_address)
+        print "\tto:", destination_address
 except:
     sys.exit()
-
-while True:
-    data, address = sock.recvfrom(SOCKET_SIZE)
-    print "Message:", data, "\n\tfrom:", address
-    sent = sock.sendto(data, destination_address)
-    print "\tto:", destination_address
+finally:
+    sock.close()
--- part1/source.py
+++ part1/source.py
@@ -3,35 +3,39 @@ import sys
 import datetime as dt
 from time import sleep
 
+# Initilaze buffer size for socket
 SOCKET_SIZE = 1024
 
-# Create a TCP/IP socket
+# Create socket with TCP connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Check whether we get expected arguments.If we do not get, exit from script
+# We need to get broker IP and port number
+# Note that we gave this arguments in our script which executes all nodes  
 if len(sys.argv) < 3:
     print "Expected Arguments:", sys.argv[0], "<BROKER-IP>", "<BROKER-PORT>"
     sys.exit()
 
+# Assign argv variables to broker address
 broker_address = (sys.argv[1], int(sys.argv[2]))
 
+# Try-except block for connection socket to broker address 
+# In case of exception, exit from script
 try:
     sock.connect(broker_address)
-except:
-    print "Connection Error:", broker_address
-    sys.exit()
 
-try:
-    #while True:
     for i in range(0,1000):
-        #message = raw_input("Message: ")
-        message = str(i)
+        message = str(i)    # = raw_input("Message: ") //alternative
+        # Get the current time 
         date_msg = dt.datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S.%f")
-
-        # Send data
+        #  Send data to broker
         sock.sendall(message + "|" + date_msg)
         print "\tSending:", message, "(%d)" % sys.getsizeof(message), "\n\t\tto (Broker):", broker_address, "\n"
 
         sleep(0.03)
-
+except:
+    print "Connection Error:", broker_address
+    sys.exit()
+# After all done close socket and exit from script
 finally:
     sock.close()
