6f5c5e3c80a973f7fd05c680f6961860307b3fd7
Berker ACIR <berkeracir159@gmail.com>
2018-12-27 07:54:22 +0000

    Update configure_routing_tables.sh

--- part2/configure_routing_tables.sh
+++ part2/configure_routing_tables.sh
@@ -15,11 +15,11 @@ if [ -n "$USERNAME" ]; then
 		if [ -f "$SSH_KEY" ]; then
 			ssh-add $SSH_KEY
             #gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $S_PORT $USERNAME@$REMOTE \"sudo ip route add 10.0.0.0\8 via 10.10.1.1 dev eth1\""
-            gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $B_PORT $USERNAME@$REMOTE \"sudo route add -net 10.10.3.0 netmask 255.255.255.0 dev eth2; sudo route add -net 10.10.5.0 netmask 255.255.255.0 dev eth3\""
+            gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $B_PORT $USERNAME@$REMOTE \"sudo route add -net 10.10.3.0 netmask 255.255.255.0 gw 10.10.2.2 dev eth2; sudo route add -net 10.10.5.0 netmask 255.255.255.0 gw 10.10.4.2 dev eth3\""
             #gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $R1_PORT $USERNAME@$REMOTE \"sudo ip route add 10.10.2.0\24 via 10.10.2.2 dev eth1; sudo ip route add 10.10.3.0\24 via 10.10.3.1 dev eth2\""
             #gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $R2_PORT $USERNAME@$REMOTE \"sudo ip route add 10.10.4.0\24 via 10.10.4.2 dev eth1; sudo ip route add 10.10.5.0\24 via 10.10.5.1 dev eth2\""
-            gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $D_PORT $USERNAME@$REMOTE \"sudo route add -net 10.10.2.0 netmask 255.255.255.0 dev eth1; sudo route add -net 10.10.4.0 netmask 255.255.255.0 dev eth2\""
-			sudo route add -net  netmask 255.255.255.0 dev eth; 
+            gnome-terminal -x bash -c "ssh -X -t -i $SSH_KEY -p $D_PORT $USERNAME@$REMOTE \"sudo route add -net 10.10.2.0 netmask 255.255.255.0 gw 10.10.3.1 dev eth1; sudo route add -net 10.10.4.0 netmask 255.255.255.0 gw 10.10.5.1 dev eth2\""
+			#sudo route add -net  netmask 255.255.255.0 dev eth; 
 		else
 			echo "SSH Key ($2) does not exist!"
 		fi
